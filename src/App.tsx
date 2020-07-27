72733e8c33cfdef22abdaabb1c0a0c55d6e81092
Julio <july2m3@gmail.com>
2020-07-26 23:24:31 -0500

    Added content for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
+import Header from './components/header';
+import Footer from './components/footer';
+
+import './style.scss';
+
 const App = () => {
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-          </ul>
-        </nav>
-      </header>
-      <main></main>
+      <Header />
+      <main className="home" id="homie">
+        <h1 className="title">Eva Mission</h1>
+        <h3 className="sub-title">Love in Action</h3>
+      </main>
+      <section className="info">
+        <ul>
+          <li className="question">What is Eva Mission?</li>
+          <li>
+            Eva Mission is a non-profit organization run by a group of
+            volunteers. Our aim is to give back to the community from the
+            proceeds of our sponsors and sales of used goods.
+          </li>
+          <li className="question">How do we give back to the community?</li>
+          <li>
+            With proceeds from our donated garage sale items, we are able to
+            provide immediate help to needy community members and families.
+          </li>
+          <li className="question">Why do we do this?</li>
+          <li>
+            By helping others in need, we also aim to plant the seeds of faith,
+            love and hope.
+          </li>
+        </ul>
+      </section>
+      <Footer />
     </>
   );
 };
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright Eva Mission Inc 2020</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
