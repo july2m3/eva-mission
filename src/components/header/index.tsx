9d001e3a8845ed559e73eaeda783ed4ccc15736e
Julio <july2m3@gmail.com>
2020-08-06 23:13:53 -0500

    Header now shows or hides upon click

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { CLIENT_RENEG_WINDOW } from 'tls';
 
 const Header = () => {
+  const [showOrHide, setShowOrHide] = React.useState('');
+
+  const handleClick = () => {
+    const className = showOrHide === '' ? 'show' : '';
+    console.log(className);
+    setShowOrHide(className);
+  };
+
   return (
-    <header>
+    <header onClick={handleClick}>
       <nav className="navigation">
         <p className="navigation__title">Eva Mission</p>
         <p className="navigation__menu-icon">=</p>
-        <ul className="navigation__menu">
+        <ul className={`navigation__menu ${showOrHide}`}>
           <li>Home</li>
           <li>About</li>
           <li>Our Inspiration</li>
