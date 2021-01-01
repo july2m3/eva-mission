bff5b87c8f55dc1de9c76dde74b3679cab2a8a1e
Julio <july2m3@gmail.com>
2020-12-31 20:00:13 -0600

    Decreased width for main content

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,30 +3,32 @@ import React from 'react';
 const Home = () => {
   return (
     <main className="home">
-      <h2 className="home__sub-title">
-        <span className="quote">"</span>
-        Love in action
-        <span className="quote">"</span>
-      </h2>
-      <main className="info">
-        <ul>
-          <li className="question">What is Eva Mission?</li>
-          <li className="answer">
-            Eva Mission is a non-profit organization run by a group of
-            volunteers.
-          </li>
-          <li className="question">How do we help others?</li>
-          <li className="answer">
-            With proceeds from our donated garage sale items, we are able to
-            provide immediate help to needy community members and families.
-          </li>
-          <li className="question">Why do we aim to help others?</li>
-          <li className="answer">
-            By helping others in need, we also aim to plant the seeds of faith,
-            love and hope.
-          </li>
-        </ul>
-      </main>
+      <div className="home-container">
+        <h2 className="home__sub-title">
+          <span className="quote">"</span>
+          Love in action
+          <span className="quote">"</span>
+        </h2>
+        <main className="info">
+          <ul>
+            <li className="question">What is Eva Mission?</li>
+            <li className="answer">
+              Eva Mission is a non-profit organization run by a group of
+              volunteers.
+            </li>
+            <li className="question">How do we help others?</li>
+            <li className="answer">
+              With proceeds from our donated garage sale items, we are able to
+              provide immediate help to needy community members and families.
+            </li>
+            <li className="question">Why do we aim to help others?</li>
+            <li className="answer">
+              By helping others in need, we also aim to plant the seeds of
+              faith, love and hope.
+            </li>
+          </ul>
+        </main>
+      </div>
     </main>
   );
 };
